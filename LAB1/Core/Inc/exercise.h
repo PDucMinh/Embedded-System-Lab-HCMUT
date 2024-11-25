5721fd5ed7d147533def2435958a7cb690a7dd27
PDucMinh <minh.phamduc1365@hcmut.edu.vn>
2024-11-25 14:26:40 +0700

    feat: new lab 1

---
+++ LAB1/Core/Inc/exercise.h
@@ -0,0 +1,25 @@
+/*
+ * excercise.h
+ *
+ *  Created on: Sep 18, 2024
+ *      Author: Admin
+ */
+
+#ifndef INC_EXERCISE_H_
+#define INC_EXERCISE_H_
+
+#include "led.h"
+
+#include "main.h"
+
+void e1_p1(led_t *led);
+void e2_p1(led_t *led);
+void e3_p1(led_t *led1, led_t *led2, led_t *led3);
+
+void e1_p2(led_t *led1, led_t *led2, led_t *led3);
+void e2_p2();
+void e3_p2();
+void e4_p2();
+void e5_p2();
+
+#endif /* INC_EXERCISE_H_ */
